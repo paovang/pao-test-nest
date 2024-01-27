e916f4f1d1e8f9a93414e7ed05410199e607961e
paovang <vangpaovang@gmail.com>
2024-01-27 12:45:05 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -21,7 +21,7 @@ export class AppController {
     });
   }
 
-  @Get('users/:id')
+  @Get('user/:id')
   async getUserById(@Param('id') id: number): Promise<User | undefined> {
     return this.userRepository.findOne({
       where: { id },
