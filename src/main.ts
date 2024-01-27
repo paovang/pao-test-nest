be072ac4b760acf703adf57c3aac18d91287f094
paovang <vangpaovang@gmail.com>
2024-01-27 12:11:21 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
