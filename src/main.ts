cd44a50eb1e152729ace964f2b3febeb2678f306
paovang <vangpaovang@gmail.com>
2024-01-27 11:48:00 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
+
   app.setGlobalPrefix('api');
   
   await app.listen(3000);
